64aae2f8d297d343d655b644e9896c51b5a97823
Rutton Sarker <ruttonchandrasarker@gmail.com>
2021-06-21 22:45:42 +0600

    solved

---
+++ Problem-Algorithms/StairCase.java
@@ -0,0 +1,28 @@
+
+/*
+    Solved by Rutton Chandra Sarker
+    North South University
+*/
+import java.util.*;
+
+public class StairCase {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int p = 1;
+        int q = n;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - p; j++) {
+                System.out.print(" ");
+            }
+
+            for (int k = q; k < n + 1; k++) {
+                System.out.print("#");
+            }
+            System.out.println();
+            p++;
+            q--;
+        }
+    }
+}
