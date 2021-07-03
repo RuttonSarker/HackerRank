75b6bd15952b405158de60c6e0520f2e0bc24ee4
Rutton Sarker <ruttonchandrasarker@gmail.com>
2021-07-03 21:27:58 +0600

    solved

---
+++ Problem-java/Loop_I.java
@@ -0,0 +1,16 @@
+
+/*
+    Solved by Rutton Chandra Sarker
+    North South University
+*/
+import java.util.*;
+
+public class Loop_I {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " x " + i + " = " + n * i);
+        }
+    }
+}
