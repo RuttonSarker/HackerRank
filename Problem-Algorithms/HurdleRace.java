6cc1220b2af1b30a2e7dfdb102a6f42d3d91e6f7
Rutton Sarker <ruttonchandrasarker@gmail.com>
2021-07-05 21:26:21 +0600

    solved

---
+++ Problem-Algorithms/HurdleRace.java
@@ -0,0 +1,31 @@
+
+/*
+    Solved by Rutton Chandra Sarker
+    North South University
+*/
+import java.util.*;
+
+public class HurdleRace {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+        int height[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            height[i] = scanner.nextInt();
+        }
+        int max = height[0];
+        for (int i = 0; i < n - 1; i++) {
+            if (max < height[i + 1]) {
+                max = height[i + 1];
+            }
+        }
+        if (max > k) {
+            int dose = max - k;
+            System.out.println(dose);
+        } else if ((max < k) || (max == k)) {
+            System.out.println(0);
+        }
+        scanner.close();
+    }
+}
