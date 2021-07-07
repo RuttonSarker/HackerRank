55ca3db358a2cc0acc216460df29ba95f3e7579d
Rutton Sarker <ruttonchandrasarker@gmail.com>
2021-07-07 22:33:56 +0600

    solved

--- Problem-java/Loops_II.java
+++ Problem-java/Loops_II.java
@@ -10,18 +10,13 @@ public class Loops_II {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int t = scanner.nextInt();
-        int sum = 0;
         for (int i = 0; i < t; i++) {
             int a = scanner.nextInt();
             int b = scanner.nextInt();
             int n = scanner.nextInt();
-
+            int sum = a;
             for (int j = 0; j < n; j++) {
-                if (j == 0) {
-                    sum += a + b;
-                } else {
-                    sum += (int) Math.pow(2, j) * b;
-                }
+                sum += (int) Math.pow(2, j) * b;
                 System.out.print(sum + " ");
             }
             sum = 0;
@@ -29,5 +24,4 @@ public class Loops_II {
         }
         scanner.close();
     }
-
 }
