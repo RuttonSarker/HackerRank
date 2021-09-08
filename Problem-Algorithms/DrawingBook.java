30d1dab1d187486aaf60ee190beec453f971da25
Rutton Sarker <ruttonchandrasarker@gmail.com>
2021-09-08 23:36:55 +0600

    pending

---
+++ Problem-Algorithms/DrawingBook.java
@@ -0,0 +1,17 @@
+
+/*
+    Solved by Rutton Chandra Sarker
+    North South University
+*/
+import java.util.*;
+
+public class DrawingBook {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+        int p = sc.nextInt();
+        int page = p / 2;
+        System.out.println(page);
+        sc.close();
+    }
+}
