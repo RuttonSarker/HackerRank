bce093d13042d4caa5a4d0458c487ff7518fddf4
Rutton Sarker <ruttonchandrasarker@gmail.com>
2021-09-11 23:15:01 +0600

    solved

--- 30 days code challenge/LetsReview.java
+++ 30 days code challenge/LetsReview.java
@@ -8,19 +8,25 @@ import java.util.*;
 public class LetsReview {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
-        char even = ' ';
-        char odd = ' ';
-        sc.close();
+        int t = sc.nextInt();
+        sc.nextLine();
 
-        for (int i = 0; i < s.length(); i++) {
-            if (i % 2 == 0) {
-                even = s.charAt(i);
-                System.out.println(even);
-            } else {
-                odd = s.charAt(i);
-                System.out.println(odd);
+        while (t != 0) {
+
+            String s = sc.nextLine();
+            String a = "";
+            String b = "";
+
+            for (int i = 0; i < s.length(); i++) {
+                if (i % 2 == 0) {
+                    a = a + s.charAt(i);
+                } else {
+                    b = b + s.charAt(i);
+                }
             }
+            System.out.println(a + " " + b);
+            t--;
         }
+        sc.close();
     }
 }
