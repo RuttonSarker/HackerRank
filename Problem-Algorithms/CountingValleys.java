40ae5890edcef3acd6fef626d2d40b753bb95ba9
Rutton Sarker <ruttonchandrasarker@gmail.com>
2021-09-18 23:54:43 +0600

    pending

---
+++ Problem-Algorithms/CountingValleys.java
@@ -0,0 +1,16 @@
+
+/*
+    Solved by Rutton Chandra Sarker
+    North South University
+*/
+import java.util.*;
+
+public class CountingValleys {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // int steps = sc.nextInt();
+        // String path = sc.nextLine();
+        // char mypath = path.charAt();
+        sc.close();
+    }
+}
