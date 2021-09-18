f14163344b09e6e200108709bfa8a82cdbeb76bf
Rutton Sarker <ruttonchandrasarker@gmail.com>
2021-09-18 23:53:11 +0600

    pending

--- Problem-Algorithms/DrawingBook.java
+++ Problem-Algorithms/DrawingBook.java
@@ -8,10 +8,23 @@ import java.util.*;
 public class DrawingBook {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // int n = sc.nextInt();
+        int n = sc.nextInt();
         int p = sc.nextInt();
-        int page = p / 2;
-        System.out.println(page);
+        int count = 0, turn = 0;
+        for (int i = 2; i <= n - 1; i += 2) {
+            turn++;
+            if (p == 1) {
+                count = 0;
+                break;
+            } else if (i == p || i + 1 == p) {
+                count += turn;
+                break;
+            }
+        }
+        for (int i = n; i > 2; i--) {
+            
+        }
+        System.out.println(count);
         sc.close();
     }
 }
