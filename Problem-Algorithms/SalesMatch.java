e7151e360d8e3a7e7441e2d529640bc5e857a032
Rutton Sarker <ruttonchandrasarker@gmail.com>
2021-10-03 23:35:49 +0600

    solved**

---
+++ Problem-Algorithms/SalesMatch.java
@@ -0,0 +1,30 @@
+
+/*
+    Solved By Rutton Chandra Sarker
+    North South University
+ */
+import java.util.*;
+
+public class SalesMatch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        int pairs = 0;
+        Set<Integer> match = new HashSet<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            if (!match.contains(arr[i])) {
+                match.add(arr[i]);
+            } else {
+                pairs++;
+                match.remove(arr[i]);
+            }
+        }
+        System.out.println(pairs);
+        sc.close();
+    }
+}
