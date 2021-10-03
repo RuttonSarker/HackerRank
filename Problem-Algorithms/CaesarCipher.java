063d1eec31fdc40764dc6c8181e7517b86c527ca
Rutton Sarker <ruttonchandrasarker@gmail.com>
2021-10-03 22:53:17 +0600

    solved ***

--- Problem-Algorithms/CaesarCipher.java
+++ Problem-Algorithms/CaesarCipher.java
@@ -10,11 +10,35 @@ public class CaesarCipher {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         sc.nextLine();
-        String s[] = new String[a];
+        String s = sc.nextLine();
+        int k = sc.nextInt();
+        char b = '\0';
+        int ascii = 0, value = 0;
 
-        for (int i = 0; i < s.length; i++) {
-            s[i] = sc.nextLine();
+        for (int i = 0; i < a; i++) {
+            if (s.charAt(i) >= 65 && s.charAt(i) <= 90) {
+                ascii = s.charAt(i) + k;
+                if (ascii > 90) {
+                    value = s.charAt(i) - a;
+                    ascii = ((ascii + value) % 26) + 65;
+                    b = (char) ascii;
+                } else {
+                    b = (char) ascii;
+                }
+            } else if (s.charAt(i) >= 97 && s.charAt(i) <= 122) {
+                ascii = s.charAt(i) + k;
+                if (ascii > 122) {
+                    value = s.charAt(i) - a;
+                    ascii = ((ascii + value) % 26) + 97;
+                    b = (char) ascii;
+                } else {
+                    b = (char) ascii;
+                }
+            } else {
+                ascii = s.charAt(i);
+                b = (char) ascii;
+            }
+            System.out.print(b + "");
         }
-        sc.close();
     }
 }
