50a1f15397630842761b7e6295c095c6094063d0
Rutton Sarker <ruttonchandrasarker@gmail.com>
2021-10-08 22:41:07 +0600

    pending

---
+++ Problem-java/ValidUsername.java
@@ -0,0 +1,19 @@
+/*
+    Solved By Rutton Chandra Sarker
+    North South University
+ */
+
+import java.util.*;
+
+public class ValidUsername {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t != 0) {
+            String s = sc.nextLine();
+
+        }
+        sc.close();
+    }
+}
