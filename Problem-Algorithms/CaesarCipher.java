f1cdb5c60e93a68c9a9a0cda83845ceb63b84815
Rutton Sarker <ruttonchandrasarker@gmail.com>
2021-10-08 22:40:46 +0600

    solved***

--- Problem-Algorithms/CaesarCipher.java
+++ Problem-Algorithms/CaesarCipher.java
@@ -14,13 +14,14 @@ public class CaesarCipher {
         int k = sc.nextInt();
         char b = '\0';
         int ascii = 0, value = 0;
+        sc.close();
 
         for (int i = 0; i < a; i++) {
             if (s.charAt(i) >= 65 && s.charAt(i) <= 90) {
                 ascii = s.charAt(i) + k;
                 if (ascii > 90) {
-                    value = s.charAt(i) - a;
-                    ascii = ((ascii + value) % 26) + 65;
+                    value = s.charAt(i) - 65;
+                    ascii = ((k + value) % 26) + 65;
                     b = (char) ascii;
                 } else {
                     b = (char) ascii;
@@ -28,8 +29,8 @@ public class CaesarCipher {
             } else if (s.charAt(i) >= 97 && s.charAt(i) <= 122) {
                 ascii = s.charAt(i) + k;
                 if (ascii > 122) {
-                    value = s.charAt(i) - a;
-                    ascii = ((ascii + value) % 26) + 97;
+                    value = s.charAt(i) - 97;
+                    ascii = ((k + value) % 26) + 97;
                     b = (char) ascii;
                 } else {
                     b = (char) ascii;
